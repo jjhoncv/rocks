1aca77c3a898d5638d39c52f4f9d6646b3961fb4
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2020-09-09 18:51:54 -0500

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   devtool: 'source-map',
   entry: path.join(__dirname, 'src/index.ts'),
   output: {
-    path: path.join(__dirname, 'dist')
+    path: path.join(__dirname, 'docs')
   },
   resolve: {
     extensions: ['.ts', '.js'],
